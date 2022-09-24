9980d74bf0f848eaa90861b0acb03ff1d93b375b
ajayratha4 <ajayratha4@gmail.com>
2022-09-24 17:53:03 +0530

    Added sh

---
+++ config.sh
@@ -0,0 +1,3 @@
+echo 'DB=mongodb+srv://ajayratha4:IavLlkf7tFL9BOaF@cluster0.f4gn3gm.mongodb.net/nodetest?retryWrites=true&w=majority' >> .env
+echo 'PORT=8080' >> .env
+echo 'JWS_SECRET_KEY=e857f85d-35a8-11ed-95b8-00ff42202387' >> .env
